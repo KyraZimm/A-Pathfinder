581c8008b40ab5fc150cec6e53eb9baf418b05b3
Kyra <klz245@nyu.edu>
2024-05-02 11:43:03 -0700

    basic UXML for map data so designers can see important values (WIP)

---
+++ Assets/PathfindingScripts/MapMakingTools/Editor/MapDataEditor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+#endif
+
+[CustomEditor(typeof(PathfindingMapData))]
+public class MapDataEditor : Editor {
+    public VisualTreeAsset UXML;
+
+    public override VisualElement CreateInspectorGUI() {
+        PathfindingMapData data = (PathfindingMapData)target;
+
+        VisualElement root = new VisualElement();
+        UXML.CloneTree(root);
+
+        IntegerField width = root.Q<IntegerField>("width");
+        IntegerField height = root.Q<IntegerField>("height");
+        Vector2Field cellsize = root.Q<Vector2Field>("cellsize");
+        Vector2Field origin = root.Q<Vector2Field>("origin");
+
+        width.value = data.grid.GetWidth();
+        height.value = data.grid.GetHeight();
+        cellsize.value = data.grid.GetCellSize();
+        origin.value = data.grid.GetOrigin();
+
+        return root;
+    }
+}
