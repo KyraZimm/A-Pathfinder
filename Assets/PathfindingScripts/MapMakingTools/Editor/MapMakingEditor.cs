2f2181cc84afd33a3f842cab49ac8963c107e991
Kyra <klz245@nyu.edu>
2024-05-08 13:35:41 -0700

    splitting window into multiple panels

--- Assets/PathfindingScripts/MapMakingTools/Editor/MapMakingEditor.cs
+++ Assets/PathfindingScripts/MapMakingTools/Editor/MapMakingEditor.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 
 #if UNITY_EDITOR
@@ -10,11 +9,18 @@ using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 #endif
 
-
 public class MapMakingEditor : EditorWindow {
     public VisualTreeAsset testUXML;
 
     private PathfindingMapData saveFile;
+
+    //layout
+    private IMGUIContainer editMapPanel;
+    private IMGUIContainer newMapPanel;
+    private ToolbarButton editMap;
+    private ToolbarButton newMap;
+
+    //ui controls
     private Button saveButton;
     private Button newSaveFileButton;
 
@@ -25,6 +31,15 @@ public class MapMakingEditor : EditorWindow {
 
     public void CreateGUI() {
         testUXML.CloneTree(rootVisualElement);
+
+        //set up layout controls
+        editMapPanel = rootVisualElement.Q<IMGUIContainer>("editmappanel");
+        newMapPanel = rootVisualElement.Q<IMGUIContainer>("newmappanel");
+        editMap = rootVisualElement.Q<ToolbarButton>("edit");
+        newMap = rootVisualElement.Q<ToolbarButton>("newmap");
+
+        editMap.clicked += DisplayEditPanel;
+        newMap.clicked += DisplayNewMapPanel;
 
         saveButton = rootVisualElement.Q<Button>("savemap");
         //saveButton.RegisterCallback<MouseUpEvent>((evt) => mapMaker.SaveCurrMapData());
@@ -37,8 +52,19 @@ public class MapMakingEditor : EditorWindow {
 
         PathfindingMapData file = (PathfindingMapData)saveFileField.value;
         SetNewSaveFile(file);
+
+        DisplayEditPanel();
     }
 
+    private void DisplayEditPanel() {
+        editMapPanel.style.display = DisplayStyle.Flex;
+        newMapPanel.style.display = DisplayStyle.None;
+    }
+
+    private void DisplayNewMapPanel() {
+        editMapPanel.style.display = DisplayStyle.None;
+        newMapPanel.style.display = DisplayStyle.Flex;
+    }
 
     private void OnSaveFileChangedEvent(ChangeEvent<UnityEngine.Object> evt) {
         PathfindingMapData newFile = (PathfindingMapData)evt.newValue;
