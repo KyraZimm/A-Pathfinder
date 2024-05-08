8743255f6c978721e237bf29fa364f0297de07fe
Kyra <klz245@nyu.edu>
2024-05-08 15:22:02 -0700

    map editor can generate new files

--- Assets/PathfindingScripts/MapMakingTools/Editor/MapMakingEditor.cs
+++ Assets/PathfindingScripts/MapMakingTools/Editor/MapMakingEditor.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -12,19 +12,21 @@ using UnityEditor.UIElements;
 public class MapMakingEditor : EditorWindow {
     public VisualTreeAsset testUXML;
 
-    private PathfindingMapData saveFile;
+    //file import & export
+    private PathfindingMapData file;
+    const string EXPORT_FOLDER_PATH = "Assets/MapData";
+    string newFileName;
 
     //layout
     private IMGUIContainer editMapPanel;
     private IMGUIContainer newMapPanel;
-    private ToolbarButton editMap;
-    private ToolbarButton newMap;
 
     //ui controls
     private Button saveButton;
     private Button newSaveFileButton;
+    
 
-    [MenuItem("Tools/Map Maker")] public static void ShowEditor() {
+    [MenuItem("Pathfinder Tools/Map Maker")] public static void ShowEditor() {
         EditorWindow wnd = GetWindow<MapMakingEditor>();
         wnd.titleContent = new GUIContent("Map Maker");
     }
@@ -35,23 +37,30 @@ public class MapMakingEditor : EditorWindow {
         //set up layout controls
         editMapPanel = rootVisualElement.Q<IMGUIContainer>("editmappanel");
         newMapPanel = rootVisualElement.Q<IMGUIContainer>("newmappanel");
-        editMap = rootVisualElement.Q<ToolbarButton>("edit");
-        newMap = rootVisualElement.Q<ToolbarButton>("newmap");
 
+        ToolbarButton editMap = rootVisualElement.Q<ToolbarButton>("edit");
+        ToolbarButton newMap = rootVisualElement.Q<ToolbarButton>("newmap");
         editMap.clicked += DisplayEditPanel;
         newMap.clicked += DisplayNewMapPanel;
 
         saveButton = rootVisualElement.Q<Button>("savemap");
         //saveButton.RegisterCallback<MouseUpEvent>((evt) => mapMaker.SaveCurrMapData());
         
-        newSaveFileButton = rootVisualElement.Q<Button>("createnewmap");
-        //newSaveFileButton.RegisterCallback<MouseUpEvent>((evt) => mapMaker.MakeNewSaveFile(forceNewFile: false));
+        newSaveFileButton = newMapPanel.Q<Button>("createnewmap");
+        newSaveFileButton.clicked += CreateNewMapFile;
 
+        //set up file name field
+        TextField fileNameField = newMapPanel.Q<TextField>("filename");
+        fileNameField.RegisterValueChangedCallback(OnNewFileNameChanged);
+        newFileName = fileNameField.value;
+        ValidateFileName(newFileName);
+
+        //set up map file location to save to
         ObjectField saveFileField = rootVisualElement.Q<ObjectField>("savefile");
         saveFileField.RegisterValueChangedCallback(OnSaveFileChangedEvent);
 
-        PathfindingMapData file = (PathfindingMapData)saveFileField.value;
-        SetNewSaveFile(file);
+        PathfindingMapData currFile = (PathfindingMapData)saveFileField.value;
+        SetNewSaveFile(currFile);
 
         DisplayEditPanel();
     }
@@ -66,15 +75,60 @@ public class MapMakingEditor : EditorWindow {
         newMapPanel.style.display = DisplayStyle.Flex;
     }
 
+    #region File Handling
     private void OnSaveFileChangedEvent(ChangeEvent<UnityEngine.Object> evt) {
         PathfindingMapData newFile = (PathfindingMapData)evt.newValue;
         SetNewSaveFile(newFile);
     }
 
     private void SetNewSaveFile(PathfindingMapData newFile) {
-        saveFile = newFile;
-        saveButton.SetEnabled(saveFile != null);
+        file = newFile;
+        saveButton.SetEnabled(file != null);
     }
+
+    private void OnNewFileNameChanged(ChangeEvent<string> evt) {
+        ValidateFileName(evt.newValue);
+    }
+
+    private void ValidateFileName(string candidateName) {
+        string path = EXPORT_FOLDER_PATH + "/" + candidateName + ".asset";
+        newSaveFileButton.SetEnabled(!File.Exists(path));
+    }
+
+    private void CreateNewMapFile() {
+        
+        //get values for new save file
+        int width = newMapPanel.Q<IntegerField>("width").value;
+        int height = newMapPanel.Q<IntegerField>("height").value;
+        Vector2 cellSize = newMapPanel.Q<Vector2Field>("cellsize").value;
+        Vector2 origin = newMapPanel.Q<Vector2Field>("origin").value;
+
+        //generate new save file w/ blank grid
+        PathfindingMapData newMapData = ScriptableObject.CreateInstance<PathfindingMapData>();
+        Grid<SavedPathNode> newMap = new Grid<SavedPathNode>(width, height, cellSize, origin);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                SavedPathNode nodeToSave = new SavedPathNode(x, y, true);
+                newMap.SetValueAtCoords(x, y, nodeToSave);
+            }
+        }
+        newMapData.grid = newMap;
+
+        //generate file at preferred path
+        string savePath = EXPORT_FOLDER_PATH + "/" + newFileName + ".asset";
+        AssetDatabase.CreateAsset(newMapData, savePath);
+
+        Debug.Log($"New save file {newFileName} was created at {savePath}.");
+        AssetDatabase.Refresh();
+
+        //update selected map file
+        ObjectField saveFileField = rootVisualElement.Q<ObjectField>("savefile");
+        saveFileField.value = newMapData;
+
+        //since file exists now, validate file name to diable button
+        ValidateFileName(newFileName);
+    }
+    #endregion
 
 
 }
