610fcf8036ff91e0d589ac6531d92f7723e8c8e1
Kyra <klz245@nyu.edu>
2024-05-09 11:50:52 -0700

    scene cleanup. archived old map maker

--- Assets/PathfindingScripts/MapMakingTools/ARCHIVED/PathfindingMapMaker_ARCHIVED.cs
+++ Assets/PathfindingScripts/MapMakingTools/ARCHIVED/PathfindingMapMaker_ARCHIVED.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
@@ -171,3 +171,4 @@ public class PathfindingMapMaker : MonoBehaviour {
 
 
 }
+*/
